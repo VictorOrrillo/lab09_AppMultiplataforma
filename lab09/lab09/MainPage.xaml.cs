a9294a939e4a212cc535c17fc5f82a6e0489feb2
VictorOrrillo <victor.orillo@tecsup.edu.pe>
2023-05-31 21:18:43 -0500

    Agregar archivos de proyecto.

---
+++ lab09/lab09/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace lab09
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void PanDemo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PanDemo());
+        }
+        private async void PinchDemoo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PinchDemo());
+        }
+        private async void SwipeDemo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SwipeDemo());
+        }
+        private async void TapDemo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TapDemo());
+        }
+    }
+}
